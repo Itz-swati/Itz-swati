de9efed6943709acb2eaee8496b3972ba06c1825
Itz-swati <swathiajithkumar9003@gmail.com>
2024-12-09 13:28:00 +0530

    Add files via upload

---
+++ case.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<?php
+$choice = 2;  
+switch($choice)
+{
+case 1:
+     echo "case 1";
+     break;
+case 2:
+     echo "case 2";
+     break;
+case 3:
+     echo "case 3";
+     break;
+default:
+     echo "default";
+     break;
+}
+?>
+</body>
+</html>
