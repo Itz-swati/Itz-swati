c77cadb416ac5e839a363895f96f0ddfbfb1542e
Itz-swati <swathiajithkumar9003@gmail.com>
2024-12-09 13:27:01 +0530

    Add files via upload

---
+++ number.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+    // Conditional check
+    if(5 < 3) {
+        echo "5 is less than 3";
+    } else {
+        echo "5 is not less than 3, so 3 is not greater than 5";
+    }
+?>
+</body>
+</html>
